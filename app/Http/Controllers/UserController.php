f58b3511cfa2723f96b8b4da81647d0fb1f71eaa
TuanPham309 <phamhoangtuan309201@gmail.com>
2023-10-17 14:39:39 +0700

    update api login

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,8 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\RegisterUserRequest;
+use App\Models\Department;
 use Illuminate\Http\Request;
 use App\Models\User;
+use App\Models\UserStatus;
+
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Carbon;
 
 class UserController extends Controller
 {
@@ -34,19 +40,45 @@ class UserController extends Controller
     public function create()
     {
         //
-        $user = new User();
-        
+        $user_status = UserStatus::all();
+        $departments = Department::all();
+
+        return response()->json([
+            'user_status' => $user_status,
+            'departments' => $departments
+        ]);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
-        //
+        $user = new User;
+        dd($request);
+        if ($request["department_id"] === null && $request["status_id"] === null) {
+            $user->create([
+
+                "username" => $request["username"],
+                "name" => $request["name"],
+                "email" => $request["email"],
+                "password" => Hash::make($request["password"]),
+                "department_id" => 1,
+                "status_id" => 2,
+            ]);
+        } else {
+            $user->create([
+
+                "username" => $request["username"],
+                "name" => $request["name"],
+                "email" => $request["email"],
+                "password" => Hash::make($request["password"]),
+                "department_id" => $request["department_id"],
+                "status_id" => $request["status_id"],
+            ]);
+        }
+
+        return response()->json([
+            "message" => "success",
+
+        ]);
     }
 
     /**
@@ -70,28 +102,50 @@ class UserController extends Controller
     public function edit($id)
     {
         //
+        $user = User::findOrFail($id);
+        $user_status = UserStatus::find($id);
+        $departments = Department::find($id);
+
+        return response()->json([
+            'user' => $user,
+            'user_status' => $user_status,
+            'departments' => $departments
+        ]);
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+
     public function update(Request $request, $id)
     {
         //
+        $validated = $request->validate([
+            "status_id" => "required",
+            "username" => "required|unique:users,username," . $id,
+            "name" => "required|max:255",
+            "email" => "required|email",
+            "department_id" => "required",
+        ]);
+        $user = User::find($id)->update([
+            "status_id" => $request["status_id"],
+            "username" => $request["username"],
+            "name" => $request["name"],
+            "email" => $request["email"],
+            "password" => Hash::make($request["password"]),
+            "department_id" => $request["department_id"],
+            "change_password_at" => now()
+        ]);
+        return response()->json([
+            "message" => "Success",
+
+        ]);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+
     public function destroy($id)
     {
         //
+        User::destroy($id);
+        return response()->json([
+            "message" => "Success"
+        ]);
     }
 }
